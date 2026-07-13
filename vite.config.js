8279f2ef4ff2a0248b418418e892ce2b6009a8cb
Silam <silpawar@deloitte.com.au>
2026-07-13 17:02:38 +1000

    chore: build related

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import basicSsl from '@vitejs/plugin-basic-ssl'
 
+const basePath = process.env.BASE_PATH || '/'
+
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
   return {
     plugins: [react(), basicSsl()],
-    base: '/',
+    base: basePath,
     server: {
       https: true,
       port: 3000,
